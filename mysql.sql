526dde439135457c8a7621e841f2e0416ef2a644
deepblue <gamemake@gmail.com>
2013-04-09 08:20:09 +0800

    m

--- mysql.sql
+++ mysql.sql
@@ -1,15 +1,18 @@
 
-CREATE TABLE avatar_table (
-  avatar_id int(11) NOT NULL,
-  avatar_name varchar(100) NOT NULL,
-  avatar_data blob,
-  PRIMARY KEY (avatar_id),
-  UNIQUE KEY avatar_name_index (avatar_name)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1$$
-
 CREATE TABLE user_table (
+	user_id int(11) NOT NULL,
+	user_name varchar(200) NOT NULL,
+	PRIMARY KEY (user_id),
+	UNIQUE KEY user_name_index (user_name)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE `avatar_table` (
+  avatar_id int(11) NOT NULL AUTO_INCREMENT,
   user_id int(11) NOT NULL,
-  user_name varchar(200) NOT NULL,
-  PRIMARY KEY (user_id),
-  UNIQUE KEY user_name_index (user_name)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1$$
+  name varchar(100) NOT NULL,
+  summary blob,
+  data blob,
+  PRIMARY KEY (avatar_id),
+  UNIQUE KEY avatar_name_index (name),
+  KEY avatar_userid_index (user_id)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
